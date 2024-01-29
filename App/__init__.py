c0ea73264371ec9373a9b5b3bf8879379916a63a
Michelle Zhang <michellezhang09876@gmail.com>
2024-01-28 22:54:54 -0500

    v 1.0

--- App/__init__.py
+++ App/__init__.py
@@ -6,18 +6,18 @@ from .exts import init_exts
 
 
 def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(blueprint=blog)
-    app.register_blueprint(blueprint=admin)
+    application = Flask(__name__)
+    application.register_blueprint(blueprint=blog)
+    application.register_blueprint(blueprint=admin)
 
     # database configuration 
     # db_uri = 'sqlite:///sqlite3.db' # sqlite 配置
     # db_uri = 'mysql+pymysql://root:root666666@localhost:3306/blogdb' # mysql 配置 example
     db_uri = 'mysql+pymysql://granted:root666666@blogdb.c5iqok0qmj8q.us-east-2.rds.amazonaws.com:3306/blogdb' # mysql 配置 example
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    application.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # initialize plug-in
-    init_exts(app = app)
+    init_exts(app = application)
     
-    return app
+    return application
